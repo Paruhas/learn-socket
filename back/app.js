1c122f001fbbbdd9b6bc543014ce3f3b39747d1b
Paruhas <bayulgang@hotmail.com>
2021-10-01 18:20:50 +0700

    add nickname, whoSend, connect, disconnect, countMember Feature

--- back/app.js
+++ back/app.js
@@ -7,19 +7,48 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const PORT = 2000;
 
 app.get("/", (req, res, next) => {
   res.sendFile(__dirname + "/app.html");
 });
 
+const Users = [];
+
 io.on("connection", (socket) => {
-  console.log("user connect");
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  let thisUser;
+
+  // console.log(socket);
+
+  socket.on("send-nickname", function (nickname) {
+    socket.nickname = nickname;
+    io.emit("user connect", socket.nickname);
+
+    thisUser = socket.nickname;
+    Users.push(socket.nickname);
+
+    console.log(`user connect name: ${socket.nickname}`);
+    console.log(`User in this room ${Users.length}`);
   });
+
+  socket.on("new message", (data) => {
+    // console.log(data);
+    io.emit("chat message", { msg: data.msg, nickname: data.nickname });
+  });
+
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    io.emit("user disconnect", thisUser);
+
+    const disUserIndex = Users.indexOf(thisUser);
+    if (disUserIndex > -1) {
+      Users.splice(disUserIndex, 1);
+    }
+
+    console.log(`user ${thisUser} disconnected`);
+    console.log(`User in this room ${Users.length}`);
   });
 });
 
